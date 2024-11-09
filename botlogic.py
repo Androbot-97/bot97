1895ee2a7e1152a503b5bec2dbf0de1e782bbc2a
Androbot-97 <andresga0807@gmail.com>
2024-11-08 20:42:03 -0500

    Update botlogic.py

--- botlogic.py
+++ botlogic.py
@@ -1,21 +1,7 @@
-import discord
-from discord.ext import commands
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix='$', intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f'We have logged in as {bot.user}')
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send(f'Hola, soy un bot {bot.user}!')
-
-@bot.command()
-async def heh(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
-
-bot.run("EL TOKEN SECRETO VA AQUÍ")  
+import random
+element = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+contraseña =""
+ask =int(input ("de cuantos caracteres va a ser tu contraseña"))
+for i in range(ask):
+    contraseña += random.choice(element)
+print (contraseña)
